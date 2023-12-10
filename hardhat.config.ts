1cb5d6b9b062c3810969e18dfc19dfea47ba1c42
Rohit Ranjan <rohitrjn629@gmail.com>
2023-12-10 12:37:06 +0530

    changes deploy hash

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,8 +5,9 @@ const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
     zkEVM: {
-    url: `https://rpc.public.zkevm-test.net`,
-    accounts: ['8c2384d17cfa9fda40a1340de57e05df266d971728b8419a7a802813c6a792e7'],
+    url: `https://rpc-mumbai.maticvigil.com/`,
+    accounts: ['35c7a994392e58cb9481faa0a433d9852afa1deb3dc75da74211fd4c69e15dbb'],
+    // accounts: ['8c2384d17cfa9fda40a1340de57e05df266d971728b8419a7a802813c6a792e7']
     },
   },
   defaultNetwork: "zkEVM",
